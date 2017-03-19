c12dabdc01aedb26bd365ee95f4a3d098b9198b9
Martin <dearnley@apiomat.com>
2017-03-19 17:37:58 +0100

    #4 preparation

--- app/src/main/java/factsforfalk/theprojectx/MainActivity.java
+++ app/src/main/java/factsforfalk/theprojectx/MainActivity.java
@@ -1,14 +1,32 @@
 package factsforfalk.theprojectx;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import layout.ShoppingListAdd;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("test");
+        setContentView(R.layout.recylcer_view);
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "floatingactionbutton", Toast.LENGTH_LONG).show();
+            }
+        }));
+    }
+
+
+    public void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
     }
 }
